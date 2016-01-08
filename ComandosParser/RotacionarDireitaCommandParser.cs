31ca74bc5599c299f8714ba2f93d324bc00a5f08
FabioVasconcelos <fabiodesignerml@gmail.com>
2016-01-08 17:12:17 -0200

    Create RotacionarDireitaCommandParser.cs

---
+++ ComandosParser/RotacionarDireitaCommandParser.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using robo.Interfaces;
+using robo.Comandos;
+namespace robo.ComandosParser
+{
+    public class RotacionarDireitaCommandParser : ICommandParser
+    {
+        // Verifica se o input corresponde ao comando desejado e caso sim cria uma instância do comando propriamente dito com
+        // o método de execução.
+
+
+        public bool ReconhecerComando(string input)
+        {
+            input.ToUpper();
+            if (input.StartsWith("R"))
+                return true;
+            else return false;
+        }
+
+        public ICommand Parse(string input)
+        {
+            return new RotacionarDireitaCommand();
+
+        }
+    }
+}
